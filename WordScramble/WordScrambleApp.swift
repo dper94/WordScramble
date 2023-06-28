1586e26baaffba5fea19145e6085530fac72c57d
Diego Perdomo <diegoperdomo94@gmail.com>
2023-06-28 15:33:48 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Diego Perdomo on 28/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
